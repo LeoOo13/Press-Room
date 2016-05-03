965daf9e33ecc2f0f57e9d1f001bf6f17f620030
Germangq <german.nava@globalquark.com.mx>
2016-05-03 12:00:52 -0500

    cambios de menú desplegable
    --

--- js/custom.js
+++ js/custom.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
 		if($("body").hasClass("menu-lateral-active")){
 			$("body").removeClass("menu-lateral-active");
 			$(".navbar-fixed-top").removeClass("navbar-left");
-			$(".menu-lateral").removeClass("menu-lateral-in");	
+			$(".menu-lateral").removeClass("menu-lateral-in");
+			$(".wrapper-sup").removeClass("sup-active");	
 		}
 		else{
 			$("body").addClass("menu-lateral-active");
 			$(".navbar-fixed-top").addClass("navbar-left");
 			$(".menu-lateral").addClass("menu-lateral-in");
-
+			$(".wrapper-sup").addClass("sup-active");
 		}
 	});
 });
